4254f7fa08fd6b5c41ea38de13eb09e64f7071ae
Dan Heath <dan@heathtechnical.com>
2025-06-28 09:35:46 +0100

    Update Logo to Use PNG Image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -34,71 +34,11 @@ const Logo: React.FC<LogoProps> = ({
 
   const LogoIcon = () => (
     <div className={`${iconSizeClasses[size]} relative flex-shrink-0`}>
-      <svg
-        viewBox="0 0 100 100"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className="w-full h-full"
-      >
-        {/* Outer circle */}
-        <circle
-          cx="50"
-          cy="50"
-          r="46"
-          stroke="#EF7674"
-          strokeWidth="8"
-          fill="none"
-        />
-        
-        {/* Main flame/drop shape - outermost */}
-        <path
-          d="M50 15 C50 15, 30 30, 30 50 C30 65, 40 75, 50 75 C60 75, 70 65, 70 50 C70 30, 50 15, 50 15 Z"
-          stroke="#EF7674"
-          strokeWidth="4"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        
-        {/* Second flame layer */}
-        <path
-          d="M50 22 C50 22, 35 35, 35 50 C35 62, 42 68, 50 68 C58 68, 65 62, 65 50 C65 35, 50 22, 50 22 Z"
-          stroke="#EF7674"
-          strokeWidth="4"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        
-        {/* Third flame layer */}
-        <path
-          d="M50 29 C50 29, 40 40, 40 50 C40 58, 44 62, 50 62 C56 62, 60 58, 60 50 C60 40, 50 29, 50 29 Z"
-          stroke="#EF7674"
-          strokeWidth="4"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        
-        {/* Inner flame/drop core */}
-        <path
-          d="M50 36 C50 36, 45 44, 45 50 C45 54, 47 56, 50 56 C53 56, 55 54, 55 50 C55 44, 50 36, 50 36 Z"
-          stroke="#EF7674"
-          strokeWidth="3"
-          fill="none"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        
-        {/* Innermost drop */}
-        <ellipse
-          cx="50"
-          cy="50"
-          rx="3"
-          ry="4"
-          fill="#EF7674"
-        />
-      </svg>
+      <img
+        src="/image.png"
+        alt="FlareTracker Logo"
+        className="w-full h-full object-contain"
+      />
     </div>
   );
 
